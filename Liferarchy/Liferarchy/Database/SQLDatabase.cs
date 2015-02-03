6dbe0a61581d2ddc9e7607479b071baf091b2276
Paul Douglas <pauldouglasguitar@gmail.com>
2015-02-03 21:04:29 +0000

    Abstract database class created

---
+++ Liferarchy/Liferarchy/Database/SQLDatabase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Liferarchy.Database
+{
+    abstract class SQLDatabase
+    {
+        abstract void connect();
+    }
+}
